0f1752f4d77100d083f42f8b64bb506e2bb31e4d
Shekhar Karande <shekhar.karande@gmail.com>
2014-02-02 22:53:05 +0000

    commiting class to add int and Float

--- com.helloWorld/src/NamasteDuniya.java
+++ com.helloWorld/src/NamasteDuniya.java
@@ -1,3 +1,7 @@
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
+import ganit.Sama;
+
 
 public class NamasteDuniya {
 
@@ -7,7 +11,13 @@ public class NamasteDuniya {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Namaskar Duniya");
-
+		int first = 10;
+		int two =20;
+		
+		Sama firstSama = new Sama(first,two);
+		
+		System.out.println("the classs value in Strng " +firstSama.toString()) ;
+		System.out.println("the value of addition is "+firstSama.getSamaFloat());
 	}
 
 }
