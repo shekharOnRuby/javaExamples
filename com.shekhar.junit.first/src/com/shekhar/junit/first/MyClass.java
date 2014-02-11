229eec2f0ed4d22388d0448169c4e9afc4167df6
shekhar.karande <shekhar.karande@gamesys.co.uk>
2014-02-11 18:48:09 +0000

    Learning Junit test

---
+++ com.shekhar.junit.first/src/com/shekhar/junit/first/MyClass.java
@@ -0,0 +1,10 @@
+package com.shekhar.junit.first;
+
+public class MyClass {
+	public int multiply(int x, int y){
+		if (x>999){
+			throw new IllegalArgumentException("X should be less than 1000");
+		}
+		return x/y;
+	}
+}
---
+++ com.shekhar.junit.first/src/com/shekhar/junit/first/MyClassTest.java
@@ -0,0 +1,14 @@
+package com.shekhar.junit.first;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MyClassTest {
+
+	@Test
+	public void testMultiply() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
