7a6fd2072e7d1fdd434a6d2dd1f8b99e53ea9e18
Shekhar karande <shekhar.karande@gmail.com>
2016-04-12 15:43:19 +0100

    pushing java code

---
+++ GalToLit.java
@@ -0,0 +1,16 @@
+/*
+1-1 This program converts litres to gallons
+*/
+
+class GalToLit{
+  public static void main(String [] args){
+    double gallons;
+    double litres;
+
+    gallons = 10;
+
+    litres = gallons * 3.7854;
+
+    System.out.println(gallons + " gallons is " + litres + " litres");
+  }
+}
--- test.java
+++ test.java
@@ -1,5 +1,9 @@
-class test{
-  public static void main(String [] args){
+public class test{
+  public static void main(String[] args){
     System.out.println ("Hello World");
+    int x =224;
+    byte y = 'A';
+    //  y = x;
+    System.out.println("printing byte: " + y);
   }
 }
